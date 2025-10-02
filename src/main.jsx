3121299f09e70c8006cb0eca909172fb584446f0
DillionB <dillion.babin@yahoo.com>
2025-10-01 21:19:22 -0700

    main

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,43 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App.jsx'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+
+import AppLayout from './layout/AppLayout.jsx'
+
+// Pages
+import Home from './pages/Home.jsx'
+import ShopPage from './pages/ShopPage.jsx'
+import AccountPage from './pages/AccountPage.jsx'
+import OrdersPage from './pages/OrdersPage.jsx'
+import RewardsPage from './pages/RewardsPage.jsx'
+import NotFound from './pages/NotFound.jsx'
+
+// State
+import { CartProvider } from './state/CartContext'
+
+// Global styles
 import './styles.css'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <CartProvider>
+      <BrowserRouter>
+        <AppLayout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/shop" element={<ShopPage />} />
+            <Route path="/account" element={<AccountPage />} />
+            <Route path="/orders" element={<OrdersPage />} />
+            <Route path="/rewards" element={<RewardsPage />} />
+            {/* Stripe return URLs can just land on Home (banner will show) */}
+            <Route path="/success" element={<Home />} />
+            <Route path="/canceled" element={<Home />} />
+            {/* Back-compat if someone visits /home */}
+            <Route path="/home" element={<Navigate to="/" replace />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </AppLayout>
+      </BrowserRouter>
+    </CartProvider>
+  </React.StrictMode>
 )
