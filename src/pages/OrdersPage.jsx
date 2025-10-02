4e513e1f85efa25d2c6ab58f3cb8ff2f98f34b2e
DillionB <dillion.babin@yahoo.com>
2025-10-01 21:36:48 -0700

    orders

---
+++ src/pages/OrdersPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Orders from '../components/Orders.jsx'
+
+export default function OrdersPage() {
+  return (
+    <section className="wrap">
+      <h2 className="section-title">My Orders</h2>
+      <Orders />
+    </section>
+  )
+}
