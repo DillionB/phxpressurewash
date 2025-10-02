ba2e64f27b30ef200f3bec556ed5fb954afa2d98
DillionB <dillion.babin@yahoo.com>
2025-10-01 21:31:59 -0700

    fix

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom'
 import Header from '../components/Header.jsx'
 import Footer from '../components/Footer.jsx'
 
-export default function SiteLayout() {
+export default function AppLayout() {
   return (
     <div className="min-h-screen">
       <Header />
