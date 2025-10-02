6148049074fdcf7252af956e9f602c832f04f88e
DillionB <dillion.babin@yahoo.com>
2025-10-01 21:40:15 -0700

    reward

---
+++ src/pages/RewardsPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Rewards from '../components/Rewards.jsx'
+
+export default function RewardsPage() {
+  return (
+    <section className="wrap">
+      <h2 className="section-title">My Rewards</h2>
+      <Rewards />
+    </section>
+  )
+}
