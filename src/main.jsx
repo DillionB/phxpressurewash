1bd99bab06c141078e8c48debc89ef001987f438
DillionB <dillion.babin@yahoo.com>
2025-10-01 18:29:50 -0700

    import

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { AuthProvider } from './state/AuthContext.jsx'
-import './styles.css' // if you have a global stylesheet
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
